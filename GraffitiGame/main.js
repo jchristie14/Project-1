27b42ac89795d40615eab0390200cb6b87df4c6b
John Christie <omeara.christie@gmail.com>
2016-07-22 16:39:07 -0400

    Begin making functions for cop movement

--- GraffitiGame/main.js
+++ GraffitiGame/main.js
@@ -4,30 +4,34 @@
 
 
   var $mario = $('#mario');
-  var $cop = $('.cop');
+  // var $cop = $('.cop');
   var $apt = $('#apt');
   var $body = $('body');
   var grafTest = $('.graffitiBox');
   $body.keydown(function(event){
-    if(event.which === 39){
+    if(event.which === 39){    //right key
       var $marioMov = $mario.css("left");
       $marioMov = $marioMov.substring(0, $marioMov.length-2)
       $marioMov = Number($marioMov)
-      $marioMov += 10;
-      $mario.css("left", $marioMov+"px")
-    }
+      if ($marioMov<=1048){
+        $marioMov += 10;
+        $mario.css("left", $marioMov+"px")
+        }
+  }
   })
   $body.keydown(function(event){
-    if(event.which === 37){
-      var $marioMov = $mario.css("left")
-      $marioMov = $marioMov.substring(0, $marioMov.length-2)
-      $marioMov = Number($marioMov)
+    if(event.which === 37){   //left key
+      var $marioMov = $mario.css("left");
+      $marioMov = $marioMov.substring(0, $marioMov.length-2);
+      $marioMov = Number($marioMov);
+      if ($marioMov>=360){
       $marioMov -= 10;
       $mario.css("left", $marioMov+"px")
+      }
     }
   });
     $body.keydown(function(event){
-    if(event.which === 40){
+    if(event.which === 40){  //down
       var $marioMov = $mario.css("top")
       $marioMov = $marioMov.substring(0, $marioMov.length-2)
       $marioMov = Number($marioMov)
@@ -36,7 +40,7 @@
     }
   })
   $body.keydown(function(event){
-    if(event.which === 38){
+    if(event.which === 38){  //up
       var $marioMov = $mario.css("top");
       $marioMov = $marioMov.substring(0, $marioMov.length-2)
       $marioMov = Number($marioMov)
@@ -44,21 +48,20 @@
       $mario.css("top", $marioMov+"px")
     }
   })
-//left = 37;up=38;right=39;down=40
+
 
   var collDetect = function(){
-    var copLoc = $cop.css("left")
+    var copLoc = $('.cop').css("left")
     copLoc = copLoc.substring(0, copLoc.length-2);
     copLoc = Number(copLoc)
     console.log(copLoc)
     var marLoc = $mario.css("left")
     marLoc = marLoc.substring(0, marLoc.length-2);
     marLoc = Number(marLoc)
-    console.log(marLoc)
     var marTop = $mario.css("top");
     marTop = marTop.substring(0, marTop.length-2);
     marTop = Number(marTop);
-    var copTop = $cop.css("top");
+    var copTop = $('.cop').css("top");
     copTop = copTop.substring(0, copTop.length-2);
     copTop = Number(copTop);
     if (40 >=Math.abs(marLoc-copLoc) && marTop===copTop){
@@ -68,8 +71,101 @@
     }
   }
 
-setInterval(collDetect, 1000)
+function createCop(num){
+  var cop = $('<img class="cop" id=cop'+num+' src="images/copTest.png"/>');
+  $('#apt').append(cop);
+}
 
+
+
+function locateCop(){
+  var cop = $('#cop1');
+  cop.css({"left":"824px", "top":"30px"});
+}
+
+function copDown(){
+  var cop =$('#cop1');
+  setTimeout(function() {
+      cop.css("top", "118px");
+    }, 2000);
+}
+
+function copRow1(){
+  var cop =$('#cop1');
+  var coinL = Math.floor(Math.random()*2);
+  var coinT = Math.floor(Math.random()*2);
+  if (coinL===1){console.log('left');
+}
+  else console.log('right; not yet')
+  }
+
+  /////cop MOVE
+
+
+function movOne(){
+      var cop = $('#cop1');
+      var copMov = cop.css("left");
+      copMov = copMov.substring(0, copMov.length-2);
+      copMov = Number(copMov);
+      if(copMov<824){
+        for(var i=copMov;i<=824;i+=146)
+          setTimeout(function(){
+          cop.css("left",i+"px");
+        }, 1000)
+      }
+      else if (copMov>824){
+        for(var i=copMov;i>=824;i-=146)
+          setTimeout(function(){
+          cop.css("left",i+"px");
+        }, 1000)
+      }
+      else cop.css("top", "206px")
+    };
+
+function copRight(){
+      var cop = $('#cop1');
+      var copMov = cop.css("left");
+      copMov = copMov.substring(0, copMov.length-2)
+      copMov = Number(copMov)
+      if (copMov<=1048){
+        copMov += 88;
+        cop.css("left", copMov+"px")
+        }
+      else copLeft
+  }
+
+    $body.keydown(function(event){
+    if(event.which === 40){  //down
+      var $marioMov = $mario.css("top")
+      $marioMov = $marioMov.substring(0, $marioMov.length-2)
+      $marioMov = Number($marioMov)
+      $marioMov += 15
+      $mario.css("top", $marioMov+"px")
+    }
+  })
+  $body.keydown(function(event){
+    if(event.which === 38){  //up
+      var $marioMov = $mario.css("top");
+      $marioMov = $marioMov.substring(0, $marioMov.length-2)
+      $marioMov = Number($marioMov)
+      $marioMov -= 15
+      $mario.css("top", $marioMov+"px")
+    }
+  })
+
+
+createCop(1);
+createCop(2);
+createCop(3);
+locateCop();
+copDown();
+setTimeout(movOne, 4000)
+
+
+
+
+
+// setInterval(collDetect, 1000);
 
   $body.keydown(function(event){
     if(event.which === 32){
@@ -77,7 +173,7 @@ setInterval(collDetect, 1000)
     }
   });
 
-  $('#one').text('TEST');
+  // $('#one').text('TEST');
 
 
 
